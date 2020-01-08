ce874e0e116126278c9816eb42ce9aeef27ad394
hilderonny <github@hildebrandt2014.de>
2020-01-08 17:59:11 +0100

    Konsolenausgabeverbessert.

---
+++ public/CONSOLE.js
@@ -0,0 +1,49 @@
+var CONSOLE = window.CONSOLE = {};
+
+function texttostring(text) {
+    if (text === undefined) return 'undefined';
+    if (text === null) return 'null';
+    if (typeof(text) === 'object') return JSON.stringify(text);
+    if (typeof(text) !== 'string') return text.toString();
+    return text;
+}
+
+CONSOLE.createinstance = function(width, height, columns, rows) {
+    var plane = BABYLON.MeshBuilder.CreatePlane('CONSOLE', { width: width, height: height }, WORLD.scene);
+    var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(plane, width * 100, height * 100);
+    var rect = new BABYLON.GUI.Rectangle();
+    rect.background = 'black';
+    advancedTexture.addControl(rect);
+    var style = advancedTexture.createStyle();
+    // Monospace has relation 1:0.55
+    style.fontSize = (width / columns * 165) + 'px';
+    style.fontFamily = "Monospace";
+    // See https://www.babylonjs-playground.com/#1CABMS#28
+    CONSOLE.textblock = new BABYLON.GUI.TextBlock();
+    CONSOLE.textblock.textHorizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+    CONSOLE.textblock.textVerticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+    CONSOLE.textblock.textWrapping = false;
+    CONSOLE.textblock.text = 'C:\>';
+    CONSOLE.textblock.color = "lightgreen";
+    CONSOLE.textblock.style = style;
+    CONSOLE.textblock.paddingLeft = CONSOLE.textblock.paddingRight = width / 4;
+    advancedTexture.addControl(CONSOLE.textblock);
+    var colpos = 0;
+    var currentline = '';
+    var lines = [];
+    plane.write = function(text) {
+        var str = texttostring(text);
+        for (var i = 0; i < str.length; i++, colpos++) {
+            var c = str[i];
+            if ((c === '\n') || (colpos >= columns)) {
+                lines.push(currentline);
+                if (lines.length >= rows) lines.splice(0, 1);
+                currentline = '';
+                colpos = 0;
+            }
+            if (c !== '\n') currentline += c;
+        }
+        CONSOLE.textblock.text = lines.join('\n') + '\n' + currentline;
+    };
+    return plane;
+};
--- public/WORLD.js
+++ public/WORLD.js
@@ -23,15 +23,6 @@ WORLD.init = function () {
     // Default intensity is 1. Let's dim the light a small amount
     light.intensity = 0.7;
 
-    // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, WORLD.scene);
-    sphere.position = new BABYLON.Vector3(-2, 0, 5);
-
-    // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-    var ground = BABYLON.Mesh.CreateGround("ground1", 1, 1, 2, WORLD.scene);
-    ground.position = new BABYLON.Vector3(2, 0, 5);
-
-
     engine.runRenderLoop(function () { WORLD.scene.render(); });
     window.addEventListener('resize', function () { engine.resize(); });
 };
